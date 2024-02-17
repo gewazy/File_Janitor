5ed09596db0d3e8d57e01abe4aef36bb7d34f083
gewazy <jurkowskidominik@gmail.com>
2024-02-17 20:14:01 +0100

    Stage 2/3

--- file-janitor.sh
+++ file-janitor.sh
@@ -1,4 +1,22 @@
 #! /bin/bash
 
-echo "File Janitor, 2024"
-echo "Powered by Bash"
+
+welcome() {
+	echo "File Janitor, 2024"
+	echo "Powered by Bash"
+}
+
+
+
+case "${1}" in 
+	"help")
+		welcome
+		echo ""
+		cat file-janitor-help.txt
+		;;
+	*)
+		welcome
+		;;
+esac
+
+
