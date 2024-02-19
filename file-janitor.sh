65c1cd8ca1ac906a11ece35e49fcf724c2507af2
gewazy <jurkowskidominik@gmail.com>
2024-02-19 21:37:13 +0100

    Stage 3/5

--- file-janitor.sh
+++ file-janitor.sh
@@ -1,21 +1,43 @@
 #! /bin/bash
-
+year=$(date +%Y)
 
 welcome() {
-	echo "File Janitor, 2024"
+	echo "File Janitor, $(date +%Y)"
 	echo "Powered by Bash"
+	echo ""
+}
+
+lister() {
+	if [[  $1 == ''  ]]; then
+		echo "Listing files in the current directory"
+		echo ''
+		ls -A1 .
+	else
+		if [ ! -e "$1"  ]; then
+			echo "$1 is not found"
+		elif [  -d "$1"  ]; then
+			echo "Listing files in $1"
+			echo ''
+			ls -A1 $1
+		else 
+			echo "$1 is not a directory"
+		fi
+	fi
 }
 
 
 
+welcome
 case "${1}" in 
 	"help")
-		welcome
-		echo ""
 		cat file-janitor-help.txt
 		;;
+
+	"list")
+		lister "${2}"
+		;;
 	*)
-		welcome
+		echo "Type file-janitor.sh help to see available options"
 		;;
 esac
 
