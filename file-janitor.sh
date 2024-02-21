d93cd6eb62a29a10a8eb8e83e01213ead3b53289
gewazy <jurkowskidominik@gmail.com>
2024-02-22 00:47:09 +0100

    Stage 4/5

--- file-janitor.sh
+++ file-janitor.sh
@@ -13,7 +13,7 @@ lister() {
 		echo ''
 		ls -A1 .
 	else
-		if [ ! -e "$1"  ]; then
+		if [  ! -e "$1"  ]; then
 			echo "$1 is not found"
 		elif [  -d "$1"  ]; then
 			echo "Listing files in $1"
@@ -26,6 +26,40 @@ lister() {
 }
 
 
+report() {
+
+	if [[  $1 == ''  ]]; then
+		echo "The current directory contains:"
+		path=.
+	else
+		if [  ! -e "$1"  ]; then
+			echo "$1 is not found"
+			exit
+		elif [  -d "$1"  ]; then
+			echo "$1 contains:"
+			path=$1
+		else
+			echo "$1 is not a directory"
+			exit
+		fi
+	fi	
+
+	count_tmp=$(find $path -maxdepth 1 -type f -name "*.tmp" | wc -l)
+	size_tmp=$(( $(find $path -maxdepth 1 -type f -name "*.tmp" -exec du -cb {} \+ | tail -1 | cut -f 1) + 0 ))
+	count_log=$(find $path -maxdepth 1 -type f -name "*.log" | wc -l)
+	size_log=$(( $(find $path -maxdepth 1 -type f -name "*.log" -exec du -cb {} \+ | tail -1 | cut -f 1) + 0 ))
+	count_py=$(find $path -maxdepth 1 -type f -name "*.py" | wc -l)
+	size_py=$(( $(find $path -maxdepth 1 -type f -name "*.py" -exec du -cb {} \+ | tail -1 | cut -f 1) + 0 ))
+
+	echo "$count_tmp tmp file(s), with total size of $size_tmp bytes"
+	echo "$count_log log flie(s), with total size of $size_log bytes"
+	echo "$count_py py file(s), with total size of $size_py bytes"
+	
+}
+
+
+
+
 
 welcome
 case "${1}" in 
@@ -35,6 +69,9 @@ case "${1}" in
 
 	"list")
 		lister "${2}"
+		;;
+	"report")
+		report "${2}" 
 		;;
 	*)
 		echo "Type file-janitor.sh help to see available options"
